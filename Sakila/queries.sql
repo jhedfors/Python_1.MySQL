31193959d06a90c187270c78c7944826a3b64f8b
Jeff Hedfors <git.j@hedfors.net>
2016-05-07 22:06:34 -0700

    first commit

---
+++ Sakila/queries.sql
@@ -0,0 +1,61 @@
+
+/*
+1. What query would you run to get all the customers inside city_id = 312? Your query should returm customer
+	first name, last name, email, and address.
+*/
+
+SELECT first_name, last_name, email, address.address, address2 from customer
+LEFT JOIN address ON address.address_id = customer.address_id
+WHERE city_id = 312;
+
+
+/*    
+2. What query would you run to get all comedy films? Your query would return film title, description, release year,
+rating, special features and genre.
+*/
+
+SELECT title, description, release_year, rating, special_features, category.name as genre from film
+LEFT JOIN film_category ON film_category.film_id = film.film_id
+LEFT JOIN category ON category.category_id = film_category.category_id
+WHERE category.name = 'comedy';
+
+/*
+3. What query would you run to get all the films joined by actor_id=5? Your query shoud return the film title,
+description and release year.
+*/
+
+SELECT title, film.description, film.release_year from film
+LEFT JOIN film_actor ON film_actor.film_id = film.film_id
+WHERE actor_id = 5;
+
+/*
+4. What query would you run to get all the customers in store_id = 1 and inside these cities (1, 42, 312 and 459)?
+Your query should return customer first name, last name, email, and address.
+*/
+
+SELECT first_name, last_name, email, address.address, address.address2 from customer
+LEFT JOIN address ON address.address_id = customer.address_id
+LEFT JOIN store ON store.store_id = customer.store_id
+WHERE customer.store_id = 1 AND (city_id = 1 OR city_id = 42 OR city_id = 312 OR city_id = 459);
+
+/*
+5. What query would you run to get all the films with a rating = G and special feature = behind the scenes, joined
+by actor_id = 15? Your query shoud return the film title, description, release year, rate and special feature.
+*/
+
+/*
+6. What query would you run to get all the actors that joined in the film_id = 369? Your query should return the
+first_name, last name and last_update of the actors.
+*/
+
+/*
+7. What query would you run to get all drama films with a rental rate of 2.99 ? Your query should return film title,
+description, release year, rating, special features and genre.
+*/
+
+/*
+8. What query would you run to get all the action films joined by SANDRA KILMER. Your query should return film
+title, description, release year, rating, special features, genre and actor's first name and last name.
+
+*/
+
